07769882ade394a0f150fbad43349ebcb067ca60
highterban <nikol.med96@gmail.com>
2025-06-24 18:51:29 +0300

    Add converter from kgs to pounds

---
+++ src/com/gmail/medvediev/homeworks/homework_1/Converter.java
@@ -0,0 +1,18 @@
+package com.gmail.medvediev.homeworks.homework_1;
+
+public class Converter {
+
+    public static final double CONV_K = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Converting app");
+
+        double kgs = 5;
+        double pounds = convKgsToPounds(kgs);
+        System.out.println("Result is " + pounds + " pounds");
+    }
+
+    private static double convKgsToPounds(double kgs) {
+        return kgs * CONV_K;
+    }
+}
