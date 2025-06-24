77c6605958a7e29770aa9621959eecfcd71674ae
highterban <nikol.med96@gmail.com>
2025-06-24 19:02:06 +0300

    Add more information for users

--- src/com/gmail/medvediev/homeworks/homework_1/Converter.java
+++ src/com/gmail/medvediev/homeworks/homework_1/Converter.java
@@ -6,7 +6,7 @@ public class Converter {
 
     public static void main(String[] args) {
         System.out.println("Converting app");
-
+        System.out.println("App for measures converting");
         double kgs = 5;
         double pounds = convKgsToPounds(kgs);
         System.out.println("Result is " + pounds + " pounds");
